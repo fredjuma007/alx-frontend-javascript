bdc31335e935df1f09ece3d1009d411e74556636
fredjuma007 <fredjuma007>
2023-07-03 17:02:59 +0200

    fyi dont push node modules lol

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  return [
+    {
+      firstName: 'Thor',
+      id: 1,
+      location: 'Asgard',
+    },
+    {
+      firstName: 'Zeus',
+      id: 2,
+      location: 'Olympus',
+    },
+    {
+      firstName: 'Alexios',
+      id: 5,
+      location: 'Sparta',
+    },
+  ];
+}
