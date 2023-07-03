b7e1f585c72b12bf551a55171ccdfd5d10362bc0
fredjuma007 <fredjuma007>
2023-07-03 18:25:39 +0200

    sort by city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,5 @@
 export default function getStudentsByLocation(students, city) {
+    // here is your damn comment
     if (Object.getPrototypeOf(students) === Array.prototype) {
         return students.filter((items) => items.location === city);
     }
